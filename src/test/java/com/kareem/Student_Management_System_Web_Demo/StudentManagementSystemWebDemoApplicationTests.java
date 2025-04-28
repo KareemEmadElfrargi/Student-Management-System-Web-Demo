cf4fea9cc511a010bcb66db2f035c55b95676063
Kareem Emad <@gmail.com>
2025-04-28 15:47:23 +0300

    Initial commit

---
+++ src/test/java/com/kareem/Student_Management_System_Web_Demo/StudentManagementSystemWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kareem.Student_Management_System_Web_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
